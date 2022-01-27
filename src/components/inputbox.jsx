a2894039a15ea9a391233120b8ca24f8e10dd96a
youngjeJO <yj01591@gmail.com>
2022-01-28 00:06:18 +0900

    list 만들기

--- src/components/inputbox.jsx
+++ src/components/inputbox.jsx
@@ -2,20 +2,34 @@ import React, { useState } from 'react';
 
 const Inputbox = (props) => {
   const [todo, setTodo] = useState('');
+  const [todolist, setTodolist] = useState([]);
+  // const clickEvent = () => setTodolist(todolist.push({ todo }));
   const onChange = (event) => setTodo(event.target.value);
-  console.log(todo);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === '') {
+      return;
+    }
+    setTodo('');
+    setTodolist((currentArray) => [todo, ...currentArray]);
+  };
+  console.log(todolist);
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
           value={todo}
           type='text'
           placeholder='what to do'
         ></input>
-
         <button>enter</button>
       </form>
+      <ul>
+        {todolist.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
